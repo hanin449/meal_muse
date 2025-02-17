6b99aa0c2d89317945324ca8c415333222371290
hanin <hanin_seid@yahoo.com>
2025-02-17 04:46:47 +0300

    server up and running

--- server/db/db.js
+++ server/db/db.js
@@ -4,7 +4,9 @@ const { Pool } = require("pg");
 // Create a connection pool using the DATABASE_URL
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.DATABASE_URL.includes("localhost") ? false : { rejectUnauthorized: false }, // Required for some cloud hosts
+  ssl: false
 });
 
 module.exports = pool;
+
+
--- server/index.js
+++ server/index.js
@@ -1,31 +1,8 @@
-
-// const express = require('express');
-// const cors = require("cors");
-// const { router: auth } = require("./routes/auth");
-// const bodyParser = require('body-parser');
-// const recipeRoute = require("./routes/recipeRoutes")
-// const app = express();
-
-// app.use(express.json(), cors());
-
-// app.use("/auth", auth);
-// app.use("/recipes", recipeRoute)
-
-// app.use(cors());  // Enable CORS for frontend requests
-// app.use(bodyParser.json());
-
-// app.get("/", (req, res) => {
-//     res.send("Welcome to the API!");
-// });
-
-// const PORT = process.env.PORT || 5000;
-// app.listen(PORT, () => console.log(`Server runnible: ${PORT}`));
-
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
-const auth = require("./routes/auth");
-const recipeRoute = require("./routes/recipeRoutes");
+const { router: auth } = require("./routes/auth");
+const { router: recipeRoute } = require("./routes/recipeRoutes");
 
 const app = express();
 
@@ -34,8 +11,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use("/auth", auth);
-app.use("/recipes", recipeRoute);
+app.use(auth);
+app.use(recipeRoute);
 
 app.get("/", (req, res) => {
     res.send("Welcome to the API!");
